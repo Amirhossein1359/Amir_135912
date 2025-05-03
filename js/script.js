1c35a7ce01fa8ac33db55656f211a4f0c0246d9c
Amirhossein1359 <amirnejad768@gmail.com>
2025-05-03 17:09:18 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+function addToCart(title, price) {
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  cart.push({ title, price });
+  localStorage.setItem('cart', JSON.stringify(cart));
+  alert(`${title} به سبد خرید اضافه شد!`);
+}
